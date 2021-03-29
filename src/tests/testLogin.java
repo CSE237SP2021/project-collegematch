837407c6200f95e6d239402c43ed3dc00f855613
abasu649 <anamika.basu@wustl.edu>
2021-03-29 11:50:08 -0700

    Issue #6, student can register and fill in user profile

---
+++ src/tests/testLogin.java
@@ -0,0 +1,19 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import collegematch.User;
+import collegematch.UserManager;
+
+
+class testLogin {
+
+	@Test
+	void testLoginSuccessful() {
+		UserManager UserManager = new UserManager();
+		User outputUserObject = UserManager.logIn("batman");
+		assertTrue(outputUserObject != null);
+	}
+
+}
